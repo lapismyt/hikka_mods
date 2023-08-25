d8e1d681596a92589a48252a02c84b8dbb709f86
LapisMYT <nikitagavrilin005@gmail.com>
2023-08-25 21:45:44 +0300

    .

---
+++ CrashStickers.py
@@ -0,0 +1,41 @@
+"""
+Licensed under GNU General Public License v3:
+https://github.com/lapismyt/hikka_mods/blob/main/LICENSE
+"""
+
+# meta developer: @LapisMods
+
+__version__ = (1, 0, 0)
+
+import logging
+from .. import loader, utils
+from hikkatl.tl.functions.messages import ForwardMessagesRequest
+import time
+
+logger = logging.getLogger(__name__)
+
+@loader.tds
+class CrashStickersMod(loader.Module):
+    """Can crash any chat you want"""
+
+    strings = {
+        "name": "CrashStickers",
+        "wait": "<spoiler>Please, wait...</spoiler>"
+    }
+
+    strings_ru = {
+        "wait": "<spoiler>Пожалуйста, подождите...</spoiler>"
+    }
+
+    def __init__(self):
+        self.name = self.strings["name"]
+
+    @loader.command(
+        ru_doc="- Отправляет 30 лагающих стикеров, которые крашают Telegram"
+    )
+    def breakchatcmd(self, message):
+        """- Sends 30 lag stickers, that crashes Telegram"""
+        await utils.answer(message, strings["wait"])
+        time.sleep(5)
+        for i in range(30):
+            pass
