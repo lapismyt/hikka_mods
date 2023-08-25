368b25fb8785639ced416240ef08a4461c4b46f4
LapisMYT <nikitagavrilin005@gmail.com>
2023-08-25 16:01:24 +0300

    .

--- UserLinker.py
+++ UserLinker.py
@@ -45,6 +45,9 @@ class UserLinkerMod(loader.Module):
     async def userlinkcmd(self, message):
         """<user_id> <text> - Make link for user"""
         args = utils.get_args_raw(message).split()
-        user_id = args[0]
-        text = " ".join(args[1:])
-        await utils.answer(message, self.strings["answer"].format(user_id, text))
+        if len(args) >= 2:
+            user_id = args[0]
+            text = " ".join(args[1:])
+            await utils.answer(message, self.strings["answer_userlink"].format(user_id, text))
+        else:
+            await utils.answer(message, self.strings["error"].format("Not enough arguments!"))
